8532e0484dfbef6263e74c4c959e51fd52abcad7
prashantkumar <prashantsundge@gmail.com>
2024-03-26 23:05:50 +0530

    Model training completed

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from cnnClassifier import logger
 from cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from cnnClassifier.pipeline.stage02_prepare_base_model import PrepareBaseModelTrainingPipeline
+from cnnClassifier.pipeline.stage03_model_training import ModelTraingPipeline
 
 STAGE_NAME = "Data Ingestion Stage"
 try:
@@ -21,6 +22,20 @@ try:
     logger.info(f">>>>>>> stage {STAGE_NAME} started <<<<<<<<<<<<")
     prepare_base_model = PrepareBaseModelTrainingPipeline()
     prepare_base_model.main()
+    logger.info(f"************************************************")
+    logger.info(f">>>>>>> stage {STAGE_NAME} Completed <<<<<<<<<<<")
+
+except Exception as e:
+     logger.exception(e)
+     raise e
+
+STAGE_NAME ="Training"
+
+try:
+    logger.info(f"***********************************************")
+    logger.info(f">>>>>>> stage {STAGE_NAME} started <<<<<<<<<<<<")
+    model_trainer = ModelTraingPipeline()
+    model_trainer.main()
     logger.info(f"************************************************")
     logger.info(f">>>>>>> stage {STAGE_NAME} Completed <<<<<<<<<<<")
 
