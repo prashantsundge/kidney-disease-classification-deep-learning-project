dea67dd6b862822b576b95770e9645ce745e1613
prashantkumar <prashantsundge@gmail.com>
2024-03-26 20:15:49 +0530

    stage prepare completed

--- src/cnnClassifier/pipeline/stage02_prepare_base_model.py
+++ src/cnnClassifier/pipeline/stage02_prepare_base_model.py
@@ -1,5 +1,5 @@
 from cnnClassifier.config.configuration import ConfigurationManager
-from cnnClassifier.components.data_ingestion import DataIngestion
+from cnnClassifier.components.prepare_base_model import PrepareBaseModel
 from cnnClassifier import logger
 
 
@@ -12,13 +12,14 @@ class PrepareBaseModelTrainingPipeline:
     def main(self):
         config = ConfigurationManager()
         prepare_base_model_config = config.get_prepare_base_model_config()
-        prepare_base_model = prepare_base_model(config= prepare_base_model_config)
+        prepare_base_model = PrepareBaseModel(config= prepare_base_model_config)
         prepare_base_model.get_base_model()
         prepare_base_model.update_base_model()
 
 if __name__ == '__main__':
     try:
         logger.info(f">>>>>>>> stage {STAGE_NAME} started <<<<<<<<")
+        obj = PrepareBaseModelTrainingPipeline()
         obj.main()
         logger.info(f">>>>>>>> stage {STAGE_NAME} completed <<<<<<")
     
